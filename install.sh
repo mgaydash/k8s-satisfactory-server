91bc73b033e99a96b28676143d12faf5222aa587
mgaydash <mitchell@gaydash.net>
2024-09-26 17:05:09 -0400

    Remove unused 'hosted_zone' settin

--- install.sh
+++ install.sh
@@ -15,7 +15,6 @@ helm upgrade \
   --install \
   --set "aws_access_key_id=${S3_ADMIN_AWS_ACCESS_KEY_ID}" \
   --set "aws_secret_access_key=${S3_ADMIN_AWS_SECRET_ACCESS_KEY}" \
-  --set "hosted_zone_id=${S3_ADMIN_AWS_HOSTED_ZONE_ID}" \
   --values ./values.yaml \
   ${RELEASE_NAME} \
   .
